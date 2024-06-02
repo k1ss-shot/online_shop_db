07c3cbefb461127542d10b0200b6e6eaf6e1c639
kairat <kairat@mancho.dev>
2024-06-03 02:10:32 +0600

    add CRUD

--- online_shop.sql
+++ online_shop.sql
@@ -1,20 +1,4 @@
 -- CREATE TABLE t_user (
--- 	first_name varchar(80),
--- 	last_name varchar(80),
--- 	adress varchar(80),
--- 	email varchar(80),
---   	phone_number varchar(80),
---   	is_active boolean
--- );
-
--- CREATE TABLE t_product (
--- 	name varchar(100),
--- 	price numeric
--- );
-
--- DROP TABLE t_user, t_product;
-
--- CREATE TABLE t_user (
 -- 	id SERIAL PRIMARY KEY,
 -- 	first_name varchar(80),
 -- 	last_name varchar(80),
@@ -39,16 +23,22 @@
 -- );
 
 -- INSERT INTO t_user(first_name, last_name, adress, email, phone_number, is_active)
--- VALUES ('Daniel', 'Osmonov', 'Moskow', 'daniel@gmail.com', '+996999888777', FALSE);
+-- VALUES ('Baha', 'Amanov', 'Naryn', 'baha@gmail.com', '+996111222333', TRUE);
 
 -- SELECT * FROM t_user;
 
 -- INSERT INTO t_product (name, price)
--- VALUES ('Джинсы Lacoste', 1000)
+-- VALUES ('Toyota Camry', 3000000);
 
--- SELECT * FROM t_product
+-- SELECT * FROM t_product;
 
-INSERT INTO t_order (user_id, product_id)
-VALUES (2, 1);
+-- INSERT INTO t_order (user_id, product_id)
+-- VALUES (3, 2);
+
+-- SELECT * FROM t_order;
+
+-- UPDATE t_user SET email = 'turdukeev@gmail.com' WHERE id=2;
+
+-- DELETE FROM t_order WHERE id=3;
 
 SELECT * FROM t_order;
